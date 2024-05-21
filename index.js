ece83b14623731083cd0ad507a3c134f02167614
Mark Jackson <markjackson956@gmail.com>
2024-05-21 14:09:36 -0400

    Added function to create image

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const addTaskBtn = document.getElementById('addTaskBtn');
 const taskContainer = document.getElementById('taskContainer');
 
 function addTask() {
+
     const taskElement = document.createElement('img');
-    taskElement.src ='https://cdn-icons-png.freepik.com/512/4804/4804180.png';
+    taskElement.src ='img/toothbrush.png';
     taskContainer.appendChild(taskElement);
+    let img = document.createElement(img)
+    img.id = "taskImg";
 }
 
 addTaskBtn.addEventListener('click', addTask);
